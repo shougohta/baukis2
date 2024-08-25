e1651ae5fdd8ef53c8d7ca201ef5018b90ae93cd
太田章吾 <ootashougo@ootashougonoMacBook-Air.local>
2024-08-25 17:09:01 +0900

    authenticator complete

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -1,21 +1,22 @@
 class Staff::SessionsController < Staff::Base
+
   def new
     if current_staff_member
       redirect_to :staff_root
     else
-      @form = Staff::LoginForm.new
+      form
       render action: "new"
     end
   end
 
   def create
-    @form = Staff::LoginForm.new(params[:staff_login_form])
+    form(params[:staff_login_form])
 
-    if @form.email.present?
-      staff_member = StaffMember.find_by("LOWER(email) = ?", @form.email.downcase)
+    if form.email.present?
+      staff_member = StaffMember.find_by("LOWER(email) = ?", form.email.downcase)
     end
 
-    if staff_member
+    if staff_authenticator(staff_member, form.password)
       session[:staff_member_id] = staff_member.id
       redirect_to :staff_root
     else
@@ -26,5 +27,15 @@ class Staff::SessionsController < Staff::Base
   def destroy
     session.delete(:staff_member_id)
     redirect_to :staff_root
+  end
+
+  private
+
+  def form(staff_login_form = nil)
+    @form ||= Staff::LoginForm.new(staff_login_form)
+  end
+
+  def staff_authenticator(staff_member, password)
+    @staff_authenticator ||= Staff::Authenticator.new(staff_member).authenticate(password)
   end
 end
